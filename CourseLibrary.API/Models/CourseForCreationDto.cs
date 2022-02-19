e10b6f9fd7b84129e73ba3ab763d0e955bd28721
Bas Martens <bmartens1988@kpnmail.nl>
2022-02-19 10:23:00 +0100

    Finished section 7

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,7 +1,23 @@
-﻿namespace CourseLibrary.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
 
-public class CourseForCreationDto
+namespace CourseLibrary.API.Models;
+
+[CourseTitleMustBeDifferentFromDescription(ErrorMessage = "Title must be different from description")]
+public class CourseForCreationDto //: IValidatableObject
 {
+    [Required(ErrorMessage = "You should fill out a title")]
+    [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
     public string Title { get; set; }
+
+    [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
     public string Description { get; set; }
+
+    // public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    // {
+    //     if (Title == Description)
+    //         yield return new ValidationResult(
+    //             "The provided description should be different from the title.",
+    //             new[] {nameof(CourseForCreationDto)});
+    // }
 }
--- CourseLibrary.API/Program.cs
+++ CourseLibrary.API/Program.cs
@@ -1,5 +1,7 @@
 using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +9,52 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers(setupActions => { setupActions.ReturnHttpNotAcceptable = true; })
-    .AddXmlDataContractSerializerFormatters();
+    .AddXmlDataContractSerializerFormatters()
+    .ConfigureApiBehaviorOptions(setupAction =>
+    {
+        setupAction.InvalidModelStateResponseFactory = context =>
+        {
+            // Create a problem details object
+            var problemDetailsFactory = context.HttpContext.RequestServices
+                .GetRequiredService<ProblemDetailsFactory>();
+            var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                context.HttpContext,
+                context.ModelState);
+
+            // Add additional info not added by default
+            problemDetails.Detail = "See the errors field for details.";
+            problemDetails.Instance = context.HttpContext.Request.Path;
+
+            // Find out which status code to use
+            var actionExecutingContext =
+                context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+            // If there are modelstate errors & all arguments were correctly
+            // found/parsed we're dealing with validation errors
+            if (context.ModelState.ErrorCount > 0 &&
+                actionExecutingContext?.ActionArguments.Count ==
+                context.ActionDescriptor.Parameters.Count)
+            {
+                problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+                problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                problemDetails.Title = "One or more validation errors occurred.";
+
+                return new UnprocessableEntityObjectResult(problemDetails)
+                {
+                    ContentTypes = {"application/problem+json"}
+                };
+            }
+
+            // If one of the arguments wasn't correctly found / couldn't be parsed
+            // we're dealing with null/unparseable input
+            problemDetails.Status = StatusCodes.Status400BadRequest;
+            problemDetails.Title = "One or more errors on input occurred.";
+            return new BadRequestObjectResult(problemDetails)
+            {
+                ContentTypes = {"application/problem+json"}
+            };
+        };
+    });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
